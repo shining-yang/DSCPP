9225d2c11095aa5b09591f8d3d03d8c064c8ac34
shining-yang <shanning_yang@cn.dlink.com>
2014-09-15 18:39:52 +0800

    update project 04_Array

---
+++ src/04_Array/Array.h
@@ -0,0 +1,81 @@
+//
+// File: Array.h
+// 
+// Shining Yang <y.s.n@live.com>, 2014/09
+//
+#pragma once
+#include <ostream>
+using namespace std;
+
+template<class T> class Array;
+
+template<class T>
+class ArrayElem {
+	friend class Array<T>;
+private:
+	//int index;
+	T data;
+};
+
+template<class T>
+class Array {
+public:
+	Array(int n);
+	~Array();
+
+public:
+	const T& operator[](int n) const;
+	T& operator[](int n);
+	void Output(ostream& os) const;
+
+private:
+	int size;
+	ArrayElem<T>* elements;
+};
+
+template<class T>
+Array<T>::Array(int n)
+{
+	size = n;
+	elements = new ArrayElem<T>[size];
+}
+
+template<class T>
+Array<T>::~Array()
+{
+	delete[] elements;
+}
+
+template<class T>
+const T& Array<T>::operator[](int n) const
+{
+	if (n < 0 || n >= size) {
+		throw new int(-1);
+	}
+
+	return elements[n].data;
+}
+
+template<class T>
+T& Array<T>::operator[](int n)
+{
+	return const_cast<T&>(
+			static_cast<const Array<T>&>(*this)[n]);
+}
+
+template<class T>
+void Array<T>::Output(ostream& os) const
+{
+	for (int i = 0; i < size; i++) {
+		os << elements[i].data << " ";
+	}
+}
+
+template<class T>
+ostream& operator<<(ostream& os, const Array<T>& a)
+{
+	a.Output(os);
+	return os;
+}
+
+
