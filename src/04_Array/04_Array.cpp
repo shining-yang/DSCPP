c0f251c589200fada30f7981f73c74118e98401b
shining-yang <shanning_yang@cn.dlink.com>
2014-09-16 12:22:12 +0800

    update the test case

--- src/04_Array/04_Array.cpp
+++ src/04_Array/04_Array.cpp
@@ -4,9 +4,19 @@
 // Shining Yang <y.s.n@live.com>, 2014/09
 //
 #include <iostream>
+#include "Array.h"
+
 using namespace std;
 
 int main(int argc, char* argv[])
 {
+	int n = 10;
+	Array<int> a(n);
+	for (int i = 0; i < n; i++) {
+		a[i] = i * i;
+	}
+
+	cout << "Array elements: \n" << a << endl;
+
 	return 0;
 }
