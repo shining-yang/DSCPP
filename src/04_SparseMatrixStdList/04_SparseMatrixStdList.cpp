8d82af7456f5ffd64cc43ac12ecaa162c5bbdabf
shining-yang <shanning_yang@cn.dlink.com>
2014-10-13 18:43:21 +0800

    add project 04_SparseMatrixStdList which implement with std::list

---
+++ src/04_SparseMatrixStdList/04_SparseMatrixStdList.cpp
@@ -0,0 +1,13 @@
+//
+// File: 04_SparseMatrixStdList.cpp
+//
+#include "SparseMatrixStdList.h"
+
+int main(int argc, char* argv[])
+{
+    CSparseMatrixStdList<int> sml;
+    cin >> sml;
+    cout << sml << endl;
+
+    return 0;
+}
