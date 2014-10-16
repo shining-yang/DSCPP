0d8d980631216206860ea012dd885c6b23e28923
shining-yang <shanning_yang@cn.dlink.com>
2014-10-16 14:21:55 +0800

    Add Project 05_StackLinkedLIst

---
+++ src/05_StackLinkedList/05_StackLinkedList.cpp
@@ -0,0 +1,12 @@
+//
+// File: 05_StackLinkedList.cpp
+// Test case for linked list stack.
+//
+#include "StackLinkedList.h"
+using namespace DSCPP::Stack;
+
+int main(int argc, char* argv[])
+{
+    StackLinkedList<char> s;
+    return 0;
+}
---
+++ src/05_StackLinkedList/StackLinkedList.h
@@ -0,0 +1,51 @@
+//
+// File: StackLinkedList.h
+//
+// Stack implemented as linked list.
+//
+// Shining Yang <y.s.n@live.com>, 2014-10-16
+//
+#pragma once
+#include "../03_SinglyLinkedList/SinglyLinkedList.h"
+
+namespace DSCPP { namespace Stack {
+
+template<typename T>
+class StackLinkedList : private Chain<T> {
+public:
+    bool IsEmpty() const {
+        return Chain<T>::IsEmpty();
+    }
+
+    bool IsFull() const {
+        ChainNode<T>* p = new ChainNode<T>();
+        if (p != NULL) {
+            delete p;
+            return true;
+        } else {
+            return true;
+        }
+    }
+
+    StackLinkedList<T>& Push(const T& x) { // internal call will throw exception if there are any
+        Insert(0, x);
+        return *this;
+    }
+
+    StackLinkedList<T>& Pop(T& x) { // internal call will throw exception if there are any
+        Delete(0, x);
+        return *this;
+    }
+
+    T Top() const {
+        if (IsEmpty()) {
+            throw new OutOfBounds();
+        }
+
+        T x;
+        Find(0, x);
+        return x;
+    }
+};
+
+}}
