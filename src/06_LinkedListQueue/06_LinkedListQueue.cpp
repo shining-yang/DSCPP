74e6ab0f79a92c1d8534cc02ca501319c9495eec
shining-yang <shanning_yang@cn.dlink.com>
2014-10-29 09:33:37 +0800

    Update test case

--- src/06_LinkedListQueue/06_LinkedListQueue.cpp
+++ src/06_LinkedListQueue/06_LinkedListQueue.cpp
@@ -7,22 +7,29 @@ using namespace DSCPP::Queue;
 int main(int argc, char* argv[])
 {
     LinkedListQueue<char> q;
+    cout << "Queue empty? " << (q.IsEmpty() ? "Yes" : "No") << endl;
+
     char c = 'A';
     int n = 0;
     for (int i = 0; i < 15; i++) {
         q.EnQueue(c + i);
     }
 
+    cout << "Queue empty? " << (q.IsEmpty() ? "Yes" : "No") << endl;
+
     //q.EnQueue('X');
     n = q.GetLength();
 
     cout << q << endl;
+    cout << "Front: " << q.Front() << ", Rear: " << q.Rear() << endl;
 
     q.DeQueue(c);
     cout << q << endl;
+    cout << "Front: " << q.Front() << ", Rear: " << q.Rear() << endl;
 
     q.DeQueue(c).EnQueue('X');
     cout << q << endl;
+    cout << "Front: " << q.Front() << ", Rear: " << q.Rear() << endl;
 
     return 0;
 }
