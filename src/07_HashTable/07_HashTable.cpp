6da6d1f5a4a43d5213391befe979b4ed690815cc
Shining Yang <y.s.n@live.com>
2014-11-22 15:28:49 +0800

    Update test case

--- src/07_HashTable/07_HashTable.cpp
+++ src/07_HashTable/07_HashTable.cpp
@@ -4,14 +4,43 @@
 //
 #include "HashTable.h"
 
+template<typename E, typename K>
+void ShowHashInfo(DSCPP::Hash::HashTable<E, K>& ht)
+{
+    cout << "Hash is full? " << (ht.IsFull() ? "Yes" : "No") << endl;
+    cout << "Hash is empty? " << (ht.IsEmpty() ? "Yes" : "No") << endl;
+    cout << "Hash:\t\t" << ht << endl;
+}
+
 int main(int argc, char* argv[])
 {
+    char dummy;
+    char ch = 'A';
     int num = 16;
-    DSCPP::Hash::HashTable<int, int> ht(16);
-    for (int i = 0; i < num; i++) {
+    DSCPP::Hash::HashTable<char, char> ht(num);
+
+    //--
+    for (char i = ch; i < ch + num; i++) {
         ht.Insert(i);
     }
+    ShowHashInfo(ht);
 
-    cout << "Hash: " << ht << endl;
+    //--
+    for (char i = ch; i < ch + num; i++) {
+        ht.Delete(i, dummy);
+    }
+    ShowHashInfo(ht);
+
+    //--
+    for (char i = ch; i < ch + num; i++) {
+        ht.Insert(i);
+    }
+    ht.Delete('E', dummy).Delete('H', dummy).Delete('B', dummy);
+    ShowHashInfo(ht);
+
+    //--
+    ht.Insert('X').Insert('Y').Insert('Z');
+    ShowHashInfo(ht);
+
     return 0;
 }
