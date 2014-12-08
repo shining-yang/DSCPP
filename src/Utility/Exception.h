92c6957371faa6045575b6f91fbbacd6c1866d48
shining-yang <shanning_yang@cn.dlink.com>
2014-12-08 17:30:51 +0800

    Comment on exceptions for better use

--- src/Utility/Exception.h
+++ src/Utility/Exception.h
@@ -9,33 +9,57 @@
 
 namespace DSCPP {
     namespace Utils {
+        // @Description:
+        //  The required data size not meet.
+        // @Example:
+        //  Dimension of array or matrix
         class SizeNotMatch {
         };
 
+        // @Description:
+        //  Usually indicates that indices are not in a valid range.
+        // @Example:
+        //  The index provided is out the range of array.
         class OutOfRange {
         };
 
+        // @Description:
+        //  This is usually similar as OutOfRange. They may be interchangeable.
         class OutOfBounds {
         };
 
+        // @Description:
+        //  Not enough memory to hold specified data or memory allocation failed
         class OutOfMemory {
         };
 
+        // @Description:
+        //  It generally indicates wrong arguments passed to constructor.
         class BadInitializer {
         };
 
+        // @Description:
+        //  The arguments passed to a method is unacceptable or invalid.
         class InvalideArgument {
         };
 
+        // @Description:
+        //  There is no such item when searching.
         class ItemNotExisted {
         };
 
+        // @Description:
+        //  Usually means the target is already existing when try to add.
         class ItemAlreadyExisted {
         };
 
+        // @Description:
+        //  When attempting to add a data element, it would go out of the upper bound.
         class ItemOverFlow {
         };
 
+        // @Description:
+        //  When attempting to remove a data element, it would go out of the lower bound.
         class ItemUnderFlow {
         };
     }
