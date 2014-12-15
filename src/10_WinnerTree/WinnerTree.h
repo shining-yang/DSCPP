0e9f7c6e660d22f69a7a9c5598208263e5b558e9
shining-yang <shanning_yang@cn.dlink.com>
2014-12-15 15:44:09 +0800

    Initiate WinnerTree class

---
+++ src/10_WinnerTree/WinnerTree.h
@@ -0,0 +1,20 @@
+//
+// File: WinnerTree.h
+// Winner tree.
+// Shining Yang <y.s.n@live.com>, 2014-12-15
+//
+#pragma once
+
+namespace DSCPP { namespace WinnerTree {
+
+template<typename T>
+class WinnerTree {
+public:
+    WinnerTree();
+    virtual ~WinnerTree();
+
+private:
+
+};
+
+}}
