ef9b2273729ec1d3e84b355b1be025df3e241ac1
shining-yang <shanning_yang@cn.dlink.com>
2014-12-18 18:38:57 +0800

    Minor updates

--- src/10_WinnerTree/WinnerTree.h
+++ src/10_WinnerTree/WinnerTree.h
@@ -3,6 +3,14 @@
 // Winner tree.
 // Shining Yang <y.s.n@live.com>, 2014-12-15
 //
+// http://www.geeksforgeeks.org/tournament-tree-and-binary-heap/
+// Tournament tree is a form of min(max) heap which is a complete binary tree.
+// Every external node represents a player and internal node represents winner.
+// In a tournament tree every internal node contains winner and every leaf node
+// contains one player.
+//
+// Tournament tree is also called Winner tree.
+//
 #pragma once
 
 namespace DSCPP { namespace WinnerTree {
